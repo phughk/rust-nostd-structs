63150f861975eb0033b37ee427be3a4a96170e6b
Przemyslaw Hugh Kaznowski <phkaznowski@gmail.com>
2023-11-30 22:22:05 +0000

    Spatial structs (#8)
    Add spatial datastructures for n dimensional points and n dimensional
    bounding boxes

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,6 @@
 #![no_std]
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+extern crate alloc;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+/// Contains the data structures that are stack allocated
+pub mod structs;
