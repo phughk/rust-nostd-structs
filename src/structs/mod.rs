e2d3637cbc8a1fbfecd1bab6268821a1f863b858
Hugh Kaznowski <phkaznowski@gmail.com>
2023-12-06 00:13:19 +0000

    Improvements to n-dim points

--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -3,24 +3,22 @@
 /// U is the base unit
 /// L is the larger unit in case of overflow
 /// S is the constant number of dimensions
-#[derive(PartialEq)]
+#[derive(PartialEq, Copy, Clone)]
 #[cfg_attr(test, derive(Debug))]
 pub struct NDimensionalPoint<U, const S: usize>
 where
-    U: Default + Copy + PartialEq,
+    U: Copy + PartialEq,
 {
     dimensions: [U; S],
 }
 
 impl<U, const S: usize> NDimensionalPoint<U, S>
 where
-    U: Default + Copy + PartialEq,
+    U: Copy + PartialEq,
 {
     /// Create a new n-dimensional spatial point
-    pub fn new() -> Self {
-        NDimensionalPoint {
-            dimensions: [U::default(); S],
-        }
+    pub fn new(vals: [U; S]) -> Self {
+        NDimensionalPoint { dimensions: vals }
     }
 
     /// Mutably set the dimension to a value for the point
@@ -34,17 +32,21 @@ where
         copied[dimension] = value;
         NDimensionalPoint { dimensions: copied }
     }
+
+    pub fn dimension(&self, dimension: usize) -> &U {
+        &self.dimensions[dimension]
+    }
 }
 
 /// An Axis Aligned Bounding Box (AABB) is a type of shape that is perfectly aligned with it's axes.
 ///
 /// Examples of such shapes include rectangles for 2D, and cubes for 3D. There is the added
 /// constraint that they can not be skewed, but must be perfectly aligned with axis.
-#[derive(PartialEq)]
+#[derive(PartialEq, Copy, Clone)]
 #[cfg_attr(test, derive(Debug))]
 pub struct AxisAlignedBoundingBox<U, const S: usize>
 where
-    U: Default + Copy + PartialEq,
+    U: Copy + PartialEq,
 {
     origin: NDimensionalPoint<U, S>,
     widths: [U; S],
@@ -52,14 +54,11 @@ where
 
 impl<U, const S: usize> AxisAlignedBoundingBox<U, S>
 where
-    U: Default + Copy + PartialEq,
+    U: Copy + PartialEq,
 {
     /// Creates a new bounding box
-    pub fn new(origin: NDimensionalPoint<U, S>) -> Self {
-        AxisAlignedBoundingBox {
-            origin: origin,
-            widths: [U::default(); S],
-        }
+    pub fn new(origin: NDimensionalPoint<U, S>, widths: [U; S]) -> Self {
+        AxisAlignedBoundingBox { origin, widths }
     }
 
     /// Returns a reference to the point of origin of this bound
@@ -76,14 +75,15 @@ where
 #[cfg(test)]
 mod test {
     use crate::structs::{AxisAlignedBoundingBox, NDimensionalPoint};
+    use alloc::boxed::Box;
+    use core::cell::{RefCell, RefMut};
+    use core::sync::atomic::{AtomicI8, Ordering};
 
     #[test]
     fn can_compare_ndimensional_point() {
-        let mut left: NDimensionalPoint<i32, 2> = NDimensionalPoint::new();
-        left.mut_set(0, 1);
-        left.mut_set(1, 2);
+        let mut left: NDimensionalPoint<i32, 2> = NDimensionalPoint::new([1, 2]);
 
-        let right = NDimensionalPoint::new().copy_set(0, 3).copy_set(1, 4);
+        let right = NDimensionalPoint::new([3, 4]);
 
         assert_ne!(left, right);
 
@@ -96,21 +96,30 @@ mod test {
 
     #[test]
     fn can_compare_aabb() {
-        let point1: NDimensionalPoint<i32, 3> = NDimensionalPoint::new()
-            .copy_set(0, 1)
-            .copy_set(1, 2)
-            .copy_set(2, 3);
-        let aabb1 = AxisAlignedBoundingBox::new(point1);
+        let point1: NDimensionalPoint<i32, 3> = NDimensionalPoint::new([1, 2, 3]);
+        let aabb1 = AxisAlignedBoundingBox::new(point1, [1, 1, 1]);
 
-        let point2: NDimensionalPoint<i32, 3> = NDimensionalPoint::new()
-            .copy_set(0, 1)
-            .copy_set(1, 2)
-            .copy_set(2, 3);
-        let mut aabb2 = AxisAlignedBoundingBox::new(point2);
+        let point2: NDimensionalPoint<i32, 3> = NDimensionalPoint::new([1, 2, 3]);
+        let mut aabb2 = AxisAlignedBoundingBox::new(point2, [1, 1, 1]);
 
         assert_eq!(aabb1, aabb2);
 
         aabb2.mut_origin().mut_set(0, 4);
         assert_ne!(aabb1, aabb2);
+    }
+
+    #[test]
+    fn can_reference() {
+        #[derive(PartialEq, Copy, Clone)]
+        struct SomeStruct {
+            x: i32,
+            y: i32,
+        }
+        let ref_cell = RefCell::new(SomeStruct { x: 5, y: 6 });
+        let point: NDimensionalPoint<&RefCell<SomeStruct>, 1> = NDimensionalPoint::new([&ref_cell]);
+        assert_eq!(point.dimension(0).borrow().x, 5);
+
+        ref_cell.borrow_mut().x = 7;
+        assert_eq!(point.dimension(0).borrow().x, 7);
     }
 }
