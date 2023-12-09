b74178595af0f7d1b18d277b4970a880264aa7ba
Przemyslaw Hugh Kaznowski <phkaznowski@gmail.com>
2023-12-09 01:37:41 +0000

    Make collision detection inclusive/exclusive (#14)

--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,10 +1,34 @@
+//! This module contains the structs that are used as primitives within various data structures
+//! and algorithms in this crate.
+//!
+//! The general idea is that some of the data structures and algorithms require a common interface
+//! for accessing them. For example spatial algorithms and data structures will naturally operate
+//! on data that is dimensional. For that, this crate provides {{structs::NDimensionalPoint}} and
+//! {{structs::AxisAlignedBoundingBox}}. Users can then create these strucutures to whatever sizes
+//! and dimensions they need, and conveniently apply them to the algorithms, without having to write
+//! Into traits for tuples or arrays.
 use core::ops::Add;
 
 /// A n-dimensional point that is used in the spatial data structures
 ///
-/// Unit is the base type used in the dimensions
-/// SumType is the larger unit in case of overflow
-/// S is the constant number of dimensions
+/// - **Unit** is the base type used in the dimensions
+/// - **SumType** is the larger unit in case of overflow
+/// - **S** is the constant number of dimensions
+///
+/// An example of how to create a point in 3D space, where x=1, y=2, z=3:
+/// ```
+/// use nostd_structs::structs::NDimensionalPoint;
+///
+/// // The precision is i32, and we specify a second i32 type, as that is the type we would need to
+/// // prevent integer overflow. Obviously, i32::max_value() + i32::max_value() would be
+/// // larger than i32. But the point is that we can specify the type not only for numbers,
+/// // but for any struct we would provide.
+/// let point = NDimensionalPoint::<i32, i32, 3>::new([1, 2, 3]);
+///
+/// assert_eq!(*point.dimension(0), 1);
+/// assert_eq!(*point.dimension(1), 2);
+/// assert_eq!(*point.dimension(2), 3);
+/// ```
 #[derive(PartialEq, Clone)]
 #[cfg_attr(test, derive(Debug))]
 pub struct NDimensionalPoint<Unit, SumType, const S: usize>
@@ -37,10 +61,12 @@ where
         NDimensionalPoint { dimensions: copied }
     }
 
+    /// Retrieve a specific dimension from the point
     pub fn dimension(&self, dimension: usize) -> &Unit {
         &self.dimensions[dimension]
     }
 
+    /// Mutably borrow a specific dimension from the point
     pub fn dimension_mut(&mut self, dimension: usize) -> &mut Unit {
         &mut self.dimensions[dimension]
     }
@@ -89,8 +115,8 @@ where
         return &mut self.origin;
     }
 
-    /// Checks if this AABB intersects with another AABB.
-    pub fn intersects(&self, other: &AxisAlignedBoundingBox<Unit, SumType, S>) -> bool {
+    /// Checks if this AABB intersects with another AABB exclusive of edges.
+    pub fn intersects_exc(&self, other: &AxisAlignedBoundingBox<Unit, SumType, S>) -> bool {
         for i in 0..S {
             let self_min = self.origin.dimension(i);
             let self_max = *self_min + self.widths[i];
@@ -104,14 +130,46 @@ where
         }
         true
     }
+
+    /// Checks if this AABB intersects with another AABB inclusive of edges.
+    pub fn intersects_inc(&self, other: &AxisAlignedBoundingBox<Unit, SumType, S>) -> bool {
+        for i in 0..S {
+            let self_min = self.origin.dimension(i);
+            let self_max = *self_min + self.widths[i];
+
+            let other_min = other.origin.dimension(i);
+            let other_max = *other_min + other.widths[i];
+
+            if self_max < (*other_min).into() || other_max < (*self_min).into() {
+                return false;
+            }
+        }
+        true
+    }
 }
 
 #[cfg(test)]
 mod test {
-    use core::cell::RefCell;
-    use core::ops::{Add, Deref};
+    use core::ops::Add;
 
     use crate::structs::{AxisAlignedBoundingBox, NDimensionalPoint};
+
+    #[derive(PartialEq, Copy, Clone, PartialOrd)]
+    struct SomeStruct {
+        x: i32,
+        y: i32,
+    }
+
+    impl Add for SomeStruct {
+        type Output = SomeStruct;
+
+        fn add(self, rhs: Self) -> Self::Output {
+            SomeStruct {
+                x: self.x + rhs.x,
+                y: self.y + rhs.y,
+            }
+        }
+    }
 
     #[test]
     fn can_compare_ndimensional_point() {
@@ -142,31 +200,44 @@ mod test {
         assert_ne!(aabb1, aabb2);
     }
 
-    #[derive(PartialEq, Copy, Clone, PartialOrd)]
-    struct SomeStruct {
-        x: i32,
-        y: i32,
-    }
-
-    impl Add for SomeStruct {
-        type Output = SomeStruct;
-
-        fn add(self, rhs: Self) -> Self::Output {
-            SomeStruct {
-                x: self.x + rhs.x,
-                y: self.y + rhs.y,
-            }
-        }
-    }
-
     #[test]
     fn can_reference() {
-        let mut some_struct = SomeStruct { x: 5, y: 6 };
+        let some_struct = SomeStruct { x: 5, y: 6 };
         let mut point: NDimensionalPoint<SomeStruct, SomeStruct, 1> =
             NDimensionalPoint::new([some_struct]);
         assert_eq!(point.dimension(0).x, 5);
 
         point.dimension_mut(0).x = 7;
         assert_eq!(point.dimension(0).x, 7);
+    }
+
+    #[test]
+    fn check_intersects_inc() {
+        let big = AxisAlignedBoundingBox::new(NDimensionalPoint::new([0, 0]), [10, 10]);
+        let medium = AxisAlignedBoundingBox::new(NDimensionalPoint::new([5, 5]), [-5, -5]);
+        let small = AxisAlignedBoundingBox::new(NDimensionalPoint::new([10, 10]), [-1, -1]);
+        let left_medium = AxisAlignedBoundingBox::new(NDimensionalPoint::new([0, 0]), [5, 5]);
+        let left_small = AxisAlignedBoundingBox::new(NDimensionalPoint::new([0, 0]), [1, 1]);
+
+        assert!(big.intersects_inc(&medium));
+        assert!(big.intersects_inc(&small));
+        assert!(big.intersects_inc(&left_medium));
+        assert!(!left_medium.intersects_inc(&small));
+        assert!(!left_small.intersects_inc(&small));
+    }
+
+    #[test]
+    fn check_intersects_exc() {
+        let big = AxisAlignedBoundingBox::new(NDimensionalPoint::new([0, 0]), [10, 10]);
+        let medium = AxisAlignedBoundingBox::new(NDimensionalPoint::new([5, 5]), [-5, -5]);
+        let small = AxisAlignedBoundingBox::new(NDimensionalPoint::new([10, 10]), [-1, -1]);
+        let left_medium = AxisAlignedBoundingBox::new(NDimensionalPoint::new([0, 0]), [5, 5]);
+        let left_small = AxisAlignedBoundingBox::new(NDimensionalPoint::new([0, 0]), [1, 1]);
+
+        assert!(!big.intersects_exc(&medium));
+        assert!(!big.intersects_exc(&small));
+        assert!(big.intersects_exc(&left_medium));
+        assert!(!left_medium.intersects_exc(&small));
+        assert!(!left_small.intersects_exc(&small));
     }
 }
