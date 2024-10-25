b26c0758c7cb7d6e7eb4eaa62e6aa39b92a77c0f
Przemyslaw Hugh Kaznowski <phkaznowski@gmail.com>
2024-10-25 23:49:13 +0100

    Fix rotating slices so they dont crash (#36)

--- src/algos/slice/rotating.rs
+++ src/algos/slice/rotating.rs
@@ -1,8 +1,14 @@
+use core::cmp::min;
+
 /// Given a slice of items, such as characters, return 2 slices that can be used to rotate the
 /// slice within a specific size.
 ///
 /// This is useful to have rotating text of a given size (such as displaying 100 characters in a
 /// 30-character wide display.
+///
+/// If the text is shorter than the width, then weird behaviour can happen, but it's tested that
+/// it won't crash. Best case is to not invoke if the text is shorter. Or modify this code to
+/// account for spacing and additional slices
 pub fn rotate_slice<T>(source: &[T], frame: usize, step: usize, width: usize) -> (&[T], &[T]) {
     // Find the animation index
     let animation_index = frame / step;
@@ -12,7 +18,7 @@ pub fn rotate_slice<T>(source: &[T], frame: usize, step: usize, width: usize) ->
     if animation_index + width < source.len() {
         (&source[animation_index..animation_index + width], &[])
     } else {
-        let remaining_width = width - (source.len() - animation_index);
+        let remaining_width = min(source.len(), width - (source.len() - animation_index));
         (&source[animation_index..], &source[..remaining_width])
     }
 }
@@ -20,6 +26,7 @@ pub fn rotate_slice<T>(source: &[T], frame: usize, step: usize, width: usize) ->
 #[cfg(test)]
 mod test {
     use crate::algos::slice::rotating::rotate_slice;
+    use std::string::String;
 
     #[test]
     fn test_rotate_slice() {
@@ -48,5 +55,14 @@ mod test {
         let (first, second) = rotate_slice(&input, 22, 2, 4);
         assert_eq!(first, "text".as_bytes());
         assert_eq!(second, "".as_bytes());
+    }
+
+    #[test]
+    fn input_shorter_than_width() {
+        let input = "This is 20 chars....".as_bytes();
+        assert_eq!(input.len(), 20);
+        let (first, second) = rotate_slice(input, 960, 30, 29);
+        assert_eq!(String::from_utf8_lossy(first), "hars....");
+        assert_eq!(String::from_utf8_lossy(second), "This is 20 chars....");
     }
 }
