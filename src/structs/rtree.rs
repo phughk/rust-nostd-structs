948c0813633f3347c5c9e072e6a3e2a9326fd81d
Hugh Kaznowski <hugh@rapidrecast.io>
2025-02-21 02:30:10 +0000

    Add failing code because can't adjust bounds of box

--- src/structs/rtree.rs
+++ src/structs/rtree.rs
@@ -22,6 +22,28 @@ where
     D: PartialOrd + Clone + Copy,
 {
     fn dimensions(&self) -> &[D; DimSz];
+    fn superset(data: &[Self]) -> Self;
+}
+
+impl<const DimSz: usize, D> Dimensionable<DimSz, D> for [D; DimSz]
+where
+    D: PartialOrd + Clone + Copy,
+{
+    fn dimensions(&self) -> &[D; DimSz] {
+        self
+    }
+
+    fn superset(data: &[Self]) -> Self {
+        let mut superset = [data[0]; DimSz];
+        for d in data.iter() {
+            for (i, dim) in d.iter().enumerate() {
+                if *dim > superset[i] {
+                    superset[i] = *dim;
+                }
+            }
+        }
+        superset
+    }
 }
 
 pub const fn calc_rtree_size(levels: usize, page_size: usize) -> usize {
@@ -59,7 +81,7 @@ where
 
     pub const fn new_const(levels: usize, page_size: usize) -> Self {
         let calc = calc_rtree_size(levels, page_size);
-        assert_eq!(calc, Sz);
+        assert!(calc == Sz, "RTree size is not equal to calculated size");
         RTree {
             data: [None; Sz],
             remaining_capacity: Sz,
@@ -73,13 +95,34 @@ where
         if self.remaining_capacity == 0 {
             return Err(());
         }
-        todo!()
+        let mut best_fit = 0;
+        for level in 0..self.levels {
+            let mut level_start = if level == 0 {
+                0
+            } else {
+                calc_rtree_size(level, self.page_size)
+            };
+
+            level_start += best_fit * self.page_size;
+            for (index, entry) in self.data[level_start..level_start + self.page_size]
+                .iter_mut()
+                .enumerate()
+            {
+                if entry.is_none() {
+                    *entry = Some(item);
+                    self.remaining_capacity -= 1;
+                    return Ok(());
+                }
+                best_fit = index;
+            }
+        }
+        unreachable!("RTree is full, but remaining_capacity is not zero");
     }
 }
 
 #[cfg(test)]
 mod test {
-    use crate::structs::rtree::calc_rtree_size;
+    use crate::structs::rtree::{calc_rtree_size, RTree};
     use std::vec::Vec;
     use std::{format, vec};
 
@@ -121,5 +164,26 @@ mod test {
                 case,
             );
         }
+    }
+
+    #[test]
+    fn test_rtree() {
+        const sz: usize = calc_rtree_size(2, 2);
+        assert_eq!(sz, 6);
+        let mut tree: RTree<sz, 2, [u16; 2], _> = RTree::new_const(2, 2);
+        assert_eq!(tree.remaining_capacity, 6);
+        tree.push([1, 2]).unwrap();
+        assert_eq!(tree.remaining_capacity, 5);
+        tree.push([2, 3]).unwrap();
+        assert_eq!(tree.remaining_capacity, 4);
+        tree.push([3, 4]).unwrap();
+        assert_eq!(tree.remaining_capacity, 3);
+        tree.push([4, 5]).unwrap();
+        assert_eq!(tree.remaining_capacity, 2);
+        tree.push([5, 6]).unwrap();
+        assert_eq!(tree.remaining_capacity, 1);
+        tree.push([6, 7]).unwrap();
+        assert_eq!(tree.remaining_capacity, 0);
+        assert!(tree.push([7, 8]).is_err());
     }
 }
