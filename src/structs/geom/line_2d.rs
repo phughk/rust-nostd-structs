f0624e77f84cb7932e64b7a7be0924f0f21e8255
Hugh Kaznowski <hugh@rapidrecast.io>
2025-03-31 16:49:02 +0100

    Some progress on line logic

--- src/structs/geom/line_2d.rs
+++ src/structs/geom/line_2d.rs
@@ -53,6 +53,39 @@ impl<T> Line2D<T> {
         }
     }
 
+    /// Returns the spatially earlier of the two points
+    pub fn earlier(&self) -> &Point2D<T>
+    where
+        T: PartialOrd,
+    {
+        &self.points[self.earlier_index()]
+    }
+
+    /// Returns the spatially later of the two points
+    pub fn later(&self) -> &Point2D<T>
+    where
+        T: PartialOrd,
+    {
+        &self.points[1 - self.earlier_index()]
+    }
+
+    fn earlier_index(&self) -> usize
+    where
+        T: PartialOrd,
+    {
+        if self.points[0].x < self.points[1].x {
+            0usize
+        } else if self.points[0].x > self.points[1].x {
+            1
+        } else {
+            if self.points[0].y < self.points[1].y {
+                0
+            } else {
+                1
+            }
+        }
+    }
+
     /// Dot product of the 2 points on the line
     pub fn dot(&self) -> T
     where
@@ -63,7 +96,7 @@ impl<T> Line2D<T> {
 
     /// Returns the remaining line and an additional line if the split happened in the middle
     /// No lines are returned if there is no overlap
-    pub fn intersection(&self, other: &Self) -> (Option<Line2D<T>>, Option<Line2D<T>>)
+    pub fn overlap(&self, other: &Self) -> (Option<Line2D<T>>, Option<Line2D<T>>)
     where
         T: Copy
             + Sub<Output = T>
@@ -72,15 +105,61 @@ impl<T> Line2D<T> {
             + Add<Output = T>
             + Neg<Output = T>
             + PartialEq
+            + PartialOrd
             + AsType<f32>,
     {
         let plane = LinearEquation::from_2_points(
             (self.points[0].x, self.points[0].y),
             (self.points[1].x, self.points[1].y),
         );
-
         let projected_start = plane.project_onto(other.points[0].x, other.points[0].y);
         let projected_end = plane.project_onto(other.points[1].x, other.points[1].y);
-        todo!()
+        let projected = Line2D::new(
+            Point2D::new(projected_start.0, projected_start.1),
+            Point2D::new(projected_end.0, projected_end.1),
+        );
+        let sizes = [
+            self.points[0].x,
+            self.points[1].x,
+            projected.points[0].x,
+            projected.points[1].x,
+        ];
+        let mut smallest = sizes[0];
+        let mut largest = sizes[0];
+        for candidate in 1..4 {
+            if sizes[candidate] < smallest {
+                smallest = sizes[candidate];
+            }
+            if sizes[candidate] > largest {
+                largest = sizes[candidate];
+            }
+        }
+        let starts_after = smallest < self.earlier().x;
+        let ends_before = largest > self.later().x;
+        match (starts_after, ends_before) {
+            (true, true) => (
+                // The entire line is inside the other line
+                Some(Line2D {
+                    points: self.points,
+                }),
+                None,
+            ),
+            (false, false) => (
+                // The other line splits this in 2
+                Some(Line2D {
+                    points: [*self.earlier(), *projected.earlier()],
+                }),
+                Some(Line2D {
+                    points: [*projected.later(), *self.later()],
+                }),
+            ),
+            (true, false) => (
+                Some(Line2D {
+                    points: [*self.earlier(), *projected.earlier()],
+                }),
+                None,
+            ),
+            (false, true) => todo!(),
+        }
     }
 }
