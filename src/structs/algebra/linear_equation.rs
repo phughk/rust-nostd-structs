dfb0b814db86e507884c6a76833cfa7d751fde33
Hugh Kaznowski <hugh@rapidrecast.io>
2025-04-01 20:10:49 +0100

    Working prop test for line projection

--- src/structs/algebra/linear_equation.rs
+++ src/structs/algebra/linear_equation.rs
@@ -36,19 +36,25 @@ impl<T> LinearEquation<T> {
     }
 
     /// Get the x value for the position y
-    pub fn x(&self, y: T) -> T
+    pub fn x(&self, y: T) -> Option<T>
     where
         T: Copy + Sub<Output = T> + Div<Output = T> + PartialEq + AsType<f32>,
     {
         match self.expr {
             LinearExpression::Slope { a, b } => {
                 if a == T::from_type(0.0) {
-                    b
+                    if b == y {
+                        // Any point on "x" is valid
+                        Some(T::from_type(0.0))
+                    } else {
+                        // No point on "x" is valid
+                        None
+                    }
                 } else {
-                    (y - b) / a
+                    Some((y - b) / a)
                 }
             }
-            LinearExpression::Vertical(x) => x,
+            LinearExpression::Vertical(x) => Some(x),
         }
     }
 
@@ -154,11 +160,31 @@ impl<T> LinearEquation<T> {
             LinearExpression::Vertical(x) => (x, y),
         }
     }
+
+    /// Translate the line by the provided x and y values
+    pub fn translate(&self, x: T, y: T) -> Self
+    where
+        T: Mul<Output = T> + Add<Output = T> + Copy + Sub<Output = T>,
+    {
+        match &self.expr {
+            LinearExpression::Slope { a, b } => {
+                let new_b = *b + y - *a * x;
+                Self {
+                    expr: LinearExpression::Slope { a: *a, b: new_b },
+                }
+            }
+            LinearExpression::Vertical(ox) => Self {
+                expr: LinearExpression::Vertical(*ox + x),
+            },
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
     use crate::structs::algebra::linear_equation::LinearEquation;
+    use proptest::prelude::*;
+    use proptest::proptest;
 
     #[test]
     fn test_project_onto() {
@@ -176,5 +202,125 @@ mod test {
         let (x, y) = vertical.project_onto(2.0, 1.0);
         assert_eq!(x, 1.0);
         assert_eq!(y, 1.0);
+    }
+
+    #[derive(Debug)]
+    struct TestInput {
+        x1: f32,
+        x2: f32,
+        y1: f32,
+        y2: f32,
+        t1: f32,
+        tx1: f32,
+        ty1: f32,
+        tx2: f32,
+        ty2: f32,
+    }
+
+    fn test_input_strategy() -> BoxedStrategy<TestInput> {
+        let prop_x1 = any::<i8>();
+        let prop_x2 = any::<i8>();
+        let prop_y1 = any::<i8>();
+        let prop_y2 = any::<i8>();
+        let prop_t1 = any::<i8>();
+        let prop_tx1 = any::<i8>();
+        let prop_ty1 = any::<i8>();
+        let prop_tx2 = any::<i8>();
+        let prop_ty2 = any::<i8>();
+        (
+            prop_x1, prop_x2, prop_y1, prop_y2, prop_t1, prop_tx1, prop_ty1, prop_tx2, prop_ty2,
+        )
+            .prop_map(|(x1, x2, y1, y2, t1, tx1, ty1, tx2, ty2)| {
+                (
+                    x1 as f32 / 10.0,
+                    x2 as f32 / 10.0,
+                    y1 as f32 / 10.0,
+                    y2 as f32 / 10.0,
+                    t1 as f32 / 10.0,
+                    tx1 as f32 / 10.0,
+                    ty1 as f32 / 10.0,
+                    tx2 as f32 / 10.0,
+                    ty2 as f32 / 10.0,
+                )
+            })
+            .prop_filter(
+                "Points on original line must be unique",
+                |(x1, x2, y1, y2, t1, tx1, ty1, tx2, ty2)| {
+                    // Points on original line must be different
+                    x1 != x2 || y1 != y2
+                },
+            )
+            .prop_filter(
+                "Points on orthogonal line must be unique",
+                |(x1, x2, y1, y2, t1, tx1, ty1, tx2, ty2)| {
+                    // Points on orthogonal line must be different
+                    tx1 != tx2 || ty1 != ty2
+                },
+            )
+            .prop_map(|(x1, x2, y1, y2, t1, tx1, ty1, tx2, ty2)| TestInput {
+                x1,
+                x2,
+                y1,
+                y2,
+                t1,
+                tx1,
+                ty1,
+                tx2,
+                ty2,
+            })
+            .boxed()
+    }
+
+    fn unit_test_project_line(input: TestInput) {
+        // We can translate any line twice across the X-axis and the projections will be the same
+        let original = LinearEquation::from_2_points((input.x1, input.y1), (input.x2, input.y2));
+        let orth = {
+            let (x, y) = match original.x(input.t1) {
+                None => (input.t1, original.y(input.t1).unwrap()),
+                Some(v) => (input.t1, v),
+            };
+            let orth = original.orthogonal_at_point(x, y);
+            orth
+        };
+        // Any 2 points on orthogonal line will have the same projection
+        // Generate points on orthogonal line
+        let (ox1, oy1) = orth.project_onto(input.tx1, input.ty1);
+        let (ox2, oy2) = orth.project_onto(input.tx2, input.ty2);
+        let (p1x, p1y) = original.project_onto(ox1, oy1);
+        let (p2x, p2y) = original.project_onto(ox2, oy2);
+        assert!(float_equal(p1x, p2x, 0.001), "{} != {}", p1x, p2x);
+        assert!(float_equal(p1y, p2y, 0.001), "{} != {}", p1y, p2y);
+    }
+
+    fn float_equal(left: f32, right: f32, precision: f32) -> bool {
+        if left.is_nan() || right.is_nan() {
+            return left.is_nan() && right.is_nan();
+        }
+        let diff = (left - right).abs();
+        diff < precision
+    }
+
+    #[test]
+    fn test_nan_project_line() {
+        let input = TestInput {
+            x1: -6.090175e18,
+            x2: 0.0,
+            y1: 3.7716197e-22,
+            y2: 0.0,
+            t1: 0.0,
+            tx1: 0.0,
+            ty1: 0.0,
+            tx2: 0.0,
+            ty2: 4.631469e-18,
+        };
+        unit_test_project_line(input);
+    }
+
+    proptest! {
+
+        #[test]
+        fn prop_test_project_line(input in test_input_strategy()) {
+            unit_test_project_line(input);
+        }
     }
 }
